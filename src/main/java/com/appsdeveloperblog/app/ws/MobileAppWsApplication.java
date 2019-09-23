e2c68663e9f49d953c2c94511e459b6a7b10fa01
Murilo <murilommms@gmail.com>
2019-09-23 10:00:46 -0300

    Configuring app to deploy in separate server

--- src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -2,11 +2,18 @@ package com.appsdeveloperblog.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
